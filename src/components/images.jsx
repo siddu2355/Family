90fdf4c2e4c60b693aba2f407220b1d66901a918
Siddu sirasanambeti <siddusirasanambeti@gmail.com>
2022-05-02 11:09:59 +0530

    Community

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 
 const Images = () => {
     return ( 
-        <h1>Images</h1>
+        <div >
+            <div style={{marginBottom:"15px"}}>
+            <img src={require("../images/tvn1.jpeg")} style={{width:500, height:300, borderRadius:"10px"}} alt="very nice" />
+            <img style={{marginLeft:500, width:300, height:400, borderTopLeftRadius:"3px"}} src={require("../images/tvn2.jpeg")} alt="very nice" />
+            </div>
+            <div style={{textAlign:"center"}}>
+            <img src={require("../images/ajanta.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/melilirecep.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/mugguru.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/lilirecep.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/sane2.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/vamsirecep.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/nare.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/vamaish.jpeg")} alt="very nice" />
+            </div>
+            <div>
+            <img src={require("../images/akilil.jpeg")} alt="very nice" />
+            </div>
+        </div>
     );
 }
  
