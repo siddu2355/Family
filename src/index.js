519d0e73376c47d088732e048038dc723bb95110
Siddu sirasanambeti <siddusirasanambeti@gmail.com>
2022-05-02 18:17:38 +0530

    Hello Raja

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Main from "./components/main";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     <BrowserRouter>
-      <App />
+      <Main />
     </BrowserRouter>
   </div>
 );
